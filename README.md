0b6275f10d84f33e994138b048891850494b3b3e
mr4k <buzzik13@gmail.com>
2021-03-12 13:25:49 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # json-credentials
-Node.js module for storing/reading credentials data in JSON file
+
+Node.js module for storing/reading credentials data in/from JSON file
+
+## How it works
+
+In the example below the module will check if the json file exists.
+If it does, the module will return the file data as an object.
+
+If the file does not exist, the user will be prompted to specify the required fields(listed in array).
+Then the provided data will be saved to a json file and returned to programm
+
+## Installing
+
+```
+npm i json-credentials
+```
+
+## Usage
+
+```
+const { getCreds } = require('json-credentials');
+(async () =>{
+  const credsData = await getCreds(['login', 'password', 'secret']);
+  console.log(`Login - ${credsData.login}, Password - ${credsData.password}`, Secret - ${credsData.secret}`)
+})();
+
+```
+
+optionally you can provide second argument wich a represents json file name
+
+```
+...
+  const credsData = await getCreds(['login', 'password', 'secret'],'my-file-name.json');
+...
+```
+
+by default, the name of the file that the module uses is credentials.json
---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+/**
+ * Module for creating/reading credentials data from JSON file.
+ * It will check if the file with provided name exists and will return its data with an object.
+ * If file does not exist, the user will be prompted to specify the required fields.
+ * Then the provided data will be saved to a json file and returned to programm
+ */
+const fs = require('fs');
+const prompt = require('prompt-sync')();
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+/**
+ * Class for managing reading/creating json file with credentials
+ */
+class JSONCredentials {
+  /**
+   * Constructor
+   * @param  {array} fields - list of fields that should present in credentials object
+   * @param  {string} filename - names of file, it should contain .json extention
+   */
+  constructor(fields, filename) {
+    this.fields = fields;
+    this.JSONfile = filename;
+  }
+  /**
+   * @return {object} - Data from .json file
+   */
+  async readJSON() {
+    const credsFileData = await readFile(this.JSONfile);
+    return JSON.parse(credsFileData);
+  }
+
+  /**
+   * Prompts the user to specify each field provided to the class
+   * @return  {object} - Specified fields
+   */
+  generateJSON() {
+    const creds = {};
+    for (const field of this.fields) {
+      creds[field] = prompt(`${field}: `);
+    }
+    fs.appendFile(this.JSONfile, JSON.stringify(creds), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    return creds;
+  }
+}
+/**
+ * A controller that receives parameters from the user and passes him a list of credentials.
+ * @param  {array} fields
+ * @param  {string} filename
+ */
+async function getCreds(fields, filename) {
+  filename = filename || 'credentials.json'; // Default filename is credentials.json
+  const creds = new JSONCredentials(fields, filename);
+  if (!fs.existsSync(filename)) {
+    console.log(`File ${filename} not found. Creating new with fields [ ${fields} ]`);
+    return creds.generateJSON();
+  }
+  const result = await creds.readJSON();
+  return result;
+}
+
+module.exports = { getCreds };
